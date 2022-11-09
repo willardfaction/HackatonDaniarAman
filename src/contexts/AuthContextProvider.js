8a7b5643eef53042c185af9b95f202a57842e528
willardfaction <dj31425364@gmail.com>
2022-11-09 19:48:40 +0600

    added login-registration-auth-provider

---
+++ src/contexts/AuthContextProvider.js
@@ -0,0 +1,46 @@
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export const authContext = React.createContext();
+export const useAuth = () => useContext(authContext);
+
+const API = " http://localhost:8000/users";
+
+const AuthContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+
+  const register = async (username, password) => {
+    let formData = new FormData();
+    formData.append("username", username);
+    formData.append("password", password);
+
+    try {
+      const res = await axios.post(`${API}register/`, formData);
+      console.log(res);
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const login = async (username, password, age, email) => {
+    let formData = new FormData();
+    formData.append("username", username);
+    formData.append("password", password);
+    formData.append("age", age);
+    formData.append("email", email);
+  };
+
+  return (
+    <authContext.Provider
+      value={{
+        register,
+        login,
+      }}>
+      {children}
+    </authContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuth } from "../contexts/AuthContextProvider";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  <h1>Login Page</h1>;
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login } = useAuth();
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+
+      <button onClick={() => login(username, password)}>Login</button>
+    </>
+  );
 };
 
 export default LoginPage;
