62cdbebcbc257d04e1f7a1bdb8644a1510cdf23a
noizziday <danya.doulbekov@gmail.com>
2022-11-09 20:25:54 +0600

    add product logic finished

--- src/components/Products/AddProduct/AddProduct.jsx
+++ src/components/Products/AddProduct/AddProduct.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useProducts } from "../../../contexts/ProductContextProvider";
+import { useContext } from "react";
 
 const AddProduct = () => {
+  const navigate = useNavigate();
+
+  const { addProduct, getProducts } = useProducts();
+
+  const [product, setProduct] = useState({
+    name: "",
+    description: "",
+    price: "",
+    image: "",
+    type: "",
+  });
+
+  const handleInp = e => {
+    if (e.target.name === "price") {
+      let obj = {
+        ...product,
+        price: Number(e.target.value),
+      };
+      setProduct(obj);
+    } else {
+      let obj = {
+        ...product,
+        [e.target.name]: e.target.value,
+      };
+      setProduct(obj);
+    }
+  };
+
   return (
     <div
       style={{
@@ -9,11 +40,43 @@ const AddProduct = () => {
       }}>
       <h2>Add product</h2>
 
-      <input type="text" placeholder="Product name" />
-      <input type="text" placeholder="Product price" />
-      <input type="text" placeholder="Product image" />
-      <input type="text" placeholder="Product description" />
-      <button>Add product</button>
+      <input
+        type="text"
+        placeholder="Product name"
+        name="name"
+        onChange={handleInp}
+      />
+      <input
+        type="text"
+        placeholder="Product price"
+        name="price"
+        onChange={handleInp}
+      />
+      <input
+        type="text"
+        placeholder="Product image"
+        name="image"
+        onChange={handleInp}
+      />
+      <input
+        type="text"
+        placeholder="Product description"
+        name="description"
+        onChange={handleInp}
+      />
+      <input
+        type="text"
+        placeholder="Product category"
+        name="type"
+        onChange={handleInp}
+      />
+      <button
+        onClick={() => {
+          addProduct(product);
+          navigate("/products");
+        }}>
+        Add product
+      </button>
     </div>
   );
 };
