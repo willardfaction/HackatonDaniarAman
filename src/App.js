a2dc1ce7735492ec3bd6f9982f9c0b22e0576d83
willardfaction <dj31425364@gmail.com>
2022-11-09 20:22:29 +0600

    fixed registration login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../src/components/Navbar/Navbar";
+import AuthContextProvider from "./contexts/AuthContextProvider";
 import MainRoutes from "./MainRoutes";
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <MainRoutes />
+      <AuthContextProvider>
+        <Navbar />
+        <MainRoutes />
+      </AuthContextProvider>
     </>
   );
 };
