b9de988f2104373e98ddf35b0042b051cb3cebd8
willardfaction <dj31425364@gmail.com>
2022-11-14 20:36:51 +0600

    added sidebar, changed navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,15 +6,74 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContextProvider";
 import { useEffect, useState } from "react";
+import SideBar from "../../components/SideBar/SideBar";
 
 export default function ButtonAppBar() {
-  const navigate = useNavigate();
-  const { login, register, logout, letter, checkAuth, deleteUser } = useAuth();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
 
-  const [userfordel, setuserfordel] = useState(null);
+  const toggleDrawer = (anchor, open) => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = anchor => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const navigate = useNavigate();
 
   // React.useEffect(() => {
   //   if (localStorage.getItem("user")) {
@@ -22,54 +81,19 @@ export default function ButtonAppBar() {
   //     checkAuth();
   //   }
   // }, []);
-  useEffect(() => {
-    let userfordel = JSON.parse(localStorage.getItem("user"));
-    setuserfordel(userfordel);
-  }, []);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
+          <SideBar />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Game shop
           </Typography>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <button onClick={() => navigate("/products")}>List of Games</button>
-          </Typography>
-          <Button
-            color="inherit"
-            onClick={() => navigate("/register")}
-            className="register">
-            Register
-          </Button>
-          <Button
-            color="inherit"
-            onClick={() => navigate("/login")}
-            className="login">
-            Login
-          </Button>
-          <Button
-            color="inherit"
-            onClick={() => {
-              navigate("/");
-              logout();
-            }}>
-            Logout
-          </Button>
-          <Button
-            color="inherit"
-            onClick={() => deleteUser(userfordel && userfordel.id)}>
-            Delete
-          </Button>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}></Typography>
         </Toolbar>
       </AppBar>
     </Box>
